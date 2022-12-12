2f766509482644262a8542c1810d8ab233992c9f
ArtemRazdobarin <80313469+ArtemRazdobarin@users.noreply.github.com>
2022-12-13 02:45:19 +0300

    Add files via upload

---
+++ A.cpp
@@ -0,0 +1,15 @@
+﻿//#include <iostream> 
+//using namespace std;
+//
+//int main() {
+//	int m, n; cin >> m >> n;
+//	if (m == 1) {
+//		cout << n;
+//		return 0;
+//	}
+//	if (n == 1) {
+//		cout << m;
+//		return 0;
+//	}
+//	cout << (2 * m + 2 * (n - 2));
+//}
